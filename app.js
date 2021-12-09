8d2af3b20bdf41a441f4e277a4ce6e9d2a5b059e
AnshMaity <44673320+AnshumanMaity@users.noreply.github.com>
2021-12-10 00:36:38 +0530

    fix: fixed empty input validation

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const outputArea=document.querySelector("#output");
 function onClickCheckButton(){
     console.log("Click is working");
     const birthdate=dob.value;
-    var dobAsNumber=birthdate.replaceAll("-","");
-    var dobReversed=dobAsNumber.split("").reverse().join("");
-    if(dobAsNumber==dobReversed){
-        outputArea.innerText="Wohhh !! Your Birth Day is Palindrome";
+    if(birthdate){
+        var dobAsNumber=birthdate.replaceAll("-","");
+        var dobReversed=dobAsNumber.split("").reverse().join("");
+        if(dobAsNumber==dobReversed){
+            outputArea.innerText="Wohhh !! Your Birth Day is Palindrome";
+        }
+        else{
+            outputArea.innerText="Opps !! Your Birth Day is not Palindrome";
+        }
     }
     else{
-        outputArea.innerText="Opps !! Your Birth Day is not Palindrome";
+        outputArea.innerText="Please enter a date.";
     }
 }
 
